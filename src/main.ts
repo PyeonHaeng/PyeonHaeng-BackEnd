b274ca3e2dc9e0ef9785c3878ead00ee714e9e2e
SeungHyun Hong <whi7ehyun@gmail.com>
2024-05-06 10:14:25 +0900

    :sparkles: Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('PyeonHaeng Server API')
+    .setDescription('The PyeonHaeng API description')
+    .setVersion('2.0')
+    .addTag('pyeonhaeng')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
 }
